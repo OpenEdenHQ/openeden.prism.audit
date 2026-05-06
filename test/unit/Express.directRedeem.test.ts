1917559a7cd63cc2db48122994c32cf4c704dd60
dukedu <dukemecn@gmail.com>
2026-05-06 11:06:05 +0800

    test(express): revert cases for requestDirectRedeem

--- test/unit/Express.directRedeem.test.ts
+++ test/unit/Express.directRedeem.test.ts
@@ -51,4 +51,89 @@ describe("Express - requestDirectRedeem", function () {
       );
     });
   });
+
+  describe("reverts", function () {
+    it("reverts on zero amount", async function () {
+      const { express, user1 } = await loadFixture(deployWithMint);
+      await expect(
+        express.connect(user1).requestDirectRedeem(RLUSD, 0, user1.address),
+      ).to.be.revertedWithCustomError(express, "InvalidAmount");
+    });
+
+    it("reverts on zero asset address", async function () {
+      const { express, user1 } = await loadFixture(deployWithMint);
+      await expect(
+        express
+          .connect(user1)
+          .requestDirectRedeem(
+            ethers.ZeroAddress,
+            ethers.parseUnits("100", 18),
+            user1.address,
+          ),
+      ).to.be.revertedWithCustomError(express, "InvalidAddress");
+    });
+
+    it("reverts when asset equals underlying", async function () {
+      const { express, user1 } = await loadFixture(deployWithMint);
+      const underlyingAddr = await express.underlying();
+      await expect(
+        express
+          .connect(user1)
+          .requestDirectRedeem(
+            underlyingAddr,
+            ethers.parseUnits("100", 18),
+            user1.address,
+          ),
+      ).to.be.revertedWithCustomError(express, "UnderlyingNotAllowed");
+    });
+
+    it("reverts when from is not KYC-listed", async function () {
+      const { express, whitelister, user1 } = await loadFixture(deployWithMint);
+      await express.connect(whitelister).revokeKycInBulk([user1.address]);
+
+      await expect(
+        express
+          .connect(user1)
+          .requestDirectRedeem(
+            RLUSD,
+            ethers.parseUnits("100", 18),
+            user1.address,
+          ),
+      ).to.be.revertedWithCustomError(express, "NotInKycList");
+    });
+
+    it("reverts when to is not KYC-listed", async function () {
+      const { express, user1 } = await loadFixture(deployWithMint);
+      const nonKyc = ethers.Wallet.createRandom().address;
+      await expect(
+        express
+          .connect(user1)
+          .requestDirectRedeem(RLUSD, ethers.parseUnits("100", 18), nonKyc),
+      ).to.be.revertedWithCustomError(express, "NotInKycList");
+    });
+
+    it("reverts when paused", async function () {
+      const { express, pauser, user1 } = await loadFixture(deployWithMint);
+      await express.connect(pauser).pauseRedeem();
+      await expect(
+        express
+          .connect(user1)
+          .requestDirectRedeem(
+            RLUSD,
+            ethers.parseUnits("100", 18),
+            user1.address,
+          ),
+      ).to.be.revertedWith("Pausable: Redeem paused");
+    });
+
+    it("reverts when token balance insufficient", async function () {
+      const { express, oem, user1 } = await loadFixture(deployWithMint);
+      const balance = await oem.balanceOf(user1.address);
+      await expect(
+        express
+          .connect(user1)
+          .requestDirectRedeem(RLUSD, balance + 1n, user1.address),
+      ).to.be.reverted;
+    });
+  });
 });
